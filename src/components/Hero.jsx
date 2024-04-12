a2b661af40c760b69e8c39aad9bc84c61d6f1489
podestal <luisrodriguezpodesta@gmail.com>
2024-04-12 12:31:36 -0400

    hero section

---
+++ src/components/Hero.jsx
@@ -0,0 +1,30 @@
+import ilustration from '../imgs/illustration-intro.svg'
+
+const Hero = () => {
+  return (
+    <section id='hero'>
+        {/* Flex container */}
+        <div className="container flex flex-col-reverse lg:flex-row items-center px-6 mx-auto mt-10 space-y-0">
+            {/* Left Item */}
+            <div className='flex flex-col mb-32 space-y-12 md:w-1/2'>
+                <h1 className='max-w-md text-4xl font-bold text-center md:text-5xl md:text-left'>
+                    Bring everyone together to build better products
+                </h1>
+                <p className='max-w-sm text-center text-darkGrayishBlue md:text-left'>
+                    Manages makes it simple for software teams to plan day-to-day tasks while keeping
+                    the larger team goals in view.
+                </p>
+                <div className='flex justify-center md:justify-start'>
+                    <p className="p-3 px-6 pt-2 text-white bg-brightRed rounded-full baseline hover:bg-brightRedLight">Get Started</p>
+                </div>
+            </div>
+            {/* Right Item Img */}
+            <div className='md:wd-1/2'>
+                <img src={ilustration} alt="ilustration" />
+            </div>
+        </div>
+    </section>
+  )
+}
+
+export default Hero
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -10,12 +10,11 @@ const Navbar = () => {
                 <img src={logo} alt="logo" />
             </div>
             {/* Menu */}
-            <div className="hidden lg:flex space-x-12">
+            <div className="hidden md:flex space-x-12">
                 <p className='hover:text-darkGrayishBlue'>Pricing</p>
                 <p className='hover:text-darkGrayishBlue'>Product</p>
                 <p className='hover:text-darkGrayishBlue'>About Us</p>
                 <p className='hover:text-darkGrayishBlue'>Careers</p>
-                <p className='hover:text-darkGrayishBlue'>Community</p>
             </div>
             {/* Button */}
             <p className="hidden md:block p-3 px-6 pt-2 text-white bg-brightRed rounded-full baseline hover:bg-brightRedLight">Get Started</p>
